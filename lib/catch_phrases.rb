8a5b53b0f06cc3c519b5ea800537e2ab681f95c0
Keenan Alves <keenan.alves@gmail.com>
2019-08-26 20:50:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,13 +2,13 @@ link = "It's Dangerous To Go Alone! Take This."
 phrase = "Do A Barrel Roll!"
 status = "Thank You Mario! But Our Princess Is In Another Castle!"
 
-def any_phrase(phrase = "Do A Barrel Roll!")
+def any_phrase(phrase)
     puts phrase
   end 
 
 def mario
-  phrase = "It's"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase
 end
 
