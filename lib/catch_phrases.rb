b53ea7cce57b4efd5fa2e9d22873d4c67f8909a7
Keenan Alves <keenan.alves@gmail.com>
2019-08-26 20:59:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,6 +2,10 @@ link = "It's Dangerous To Go Alone! Take This."
 phrase = "Do A Barrel Roll!"
 status = "Thank You Mario! But Our Princess Is In Another Castle!"
 
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end 
+
 def any_phrase(phrase)
     puts phrase
   end 
